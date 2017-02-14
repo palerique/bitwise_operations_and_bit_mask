57e9cdcea46c097aab643140f10a68821b8f48fd
Paulo Henrique Lerbach Rodrigues <ph@Paulos-MacBook-Pro.local>
2017-02-14 19:51:53 -0200

    Initial commit

---
+++ src/main/java/br/com/sitedoph/bitwiseopsandmask/DBSecurityException.java
@@ -0,0 +1,12 @@
+package br.com.sitedoph.bitwiseopsandmask;
+
+public class DBSecurityException extends Exception {
+
+    DBSecurityException() {
+        super();
+    }
+
+    DBSecurityException(String s) {
+        super(s);
+    }
+}
